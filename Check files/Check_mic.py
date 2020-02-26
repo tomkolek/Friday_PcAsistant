e4ab24d46301f2226a69ab4af31711cfa85a99b5
tomkolek <60440240+tomkolek@users.noreply.github.com>
2020-02-26 22:21:02 +0100

    Update Check_mic.py

--- Check files/Check_mic.py
+++ Check files/Check_mic.py
@@ -1,4 +1,4 @@
 import speech_recognition as sr
 for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+    print("Mikrofon z nazwą \"{1}\" znaleziony jako `Microphone(device_index={0})`".format(index, name))
 input("naciśnij enter")
