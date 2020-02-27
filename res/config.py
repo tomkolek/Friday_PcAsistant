5dd7e858fa71206f01b2082fce5ccb9a7e85cbb1
tomkolek <60440240+tomkolek@users.noreply.github.com>
2020-02-27 17:34:16 +0100

    Update config.py

--- res/config.py
+++ res/config.py
@@ -1,5 +1,5 @@
 softList = [
-            #['SOFT', 'Steam', ['Steama' ,'Steam'], 'D:\Programy\Steam\Steam.exe'],
+            # ['SOFT', 'Steam', ['Steama' ,'Steam'], 'D:\Programy\Steam\Steam.exe'],
             ['WEB', 'YouTube', ['youtube', 'youtuba'], 'https://www.youtube.com/'],
             ['WEB', 'Google', ['google', 'gogle'], 'https://www.google.com/']
            ]
